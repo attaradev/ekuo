399cc14282139a229afde08907c9743484c7b5c8
Mike Attara <mpyebattara@gmail.com>
2023-09-23 09:47:24 +0000

    fix ci

--- packages/nextjs/components/example-ui/ContractData.tsx
+++ packages/nextjs/components/example-ui/ContractData.tsx
@@ -1,13 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import Marquee from "react-fast-marquee";
 import { useAccount } from "wagmi";
-import {
-  useAnimationConfig,
-  useScaffoldContract,
-  useScaffoldContractRead,
-  useScaffoldEventHistory,
-  useScaffoldEventSubscriber,
-} from "~~/hooks/scaffold-eth";
+import { useScaffoldContract, useScaffoldEventHistory, useScaffoldEventSubscriber } from "~~/hooks/scaffold-eth";
 
 const MARQUEE_PERIOD_IN_SEC = 5;
 
@@ -19,17 +13,6 @@ export const ContractData = () => {
 
   const containerRef = useRef<HTMLDivElement>(null);
   const greetingRef = useRef<HTMLDivElement>(null);
-
-  const { data: totalCounter } = useScaffoldContractRead({
-    contractName: "EkuoDAO",
-    functionName: "proposalCount",
-  });
-
-  const { data: currentGreeting, isLoading: isGreetingLoading } = useScaffoldContractRead({
-    contractName: "EkuoDAO",
-    functionName: "proposals",
-    args: [BigInt(0)],
-  });
 
   useScaffoldEventSubscriber({
     contractName: "EkuoDAO",
@@ -59,10 +42,6 @@ export const ContractData = () => {
   const { data: yourContract } = useScaffoldContract({ contractName: "EkuoDAO" });
   console.log("yourContract: ", yourContract);
 
-  const { showAnimation } = useAnimationConfig(totalCounter);
-
-  const showTransition = transitionEnabled && !!currentGreeting && !isGreetingLoading;
-
   useEffect(() => {
     if (transitionEnabled && containerRef.current && greetingRef.current) {
       setMarqueeSpeed(
@@ -75,7 +54,7 @@ export const ContractData = () => {
     <div className="flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
       <div
         className={`flex flex-col max-w-md bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-4 w-full ${
-          showAnimation ? "animate-zoom" : ""
+          transitionEnabled ? "animate-pulse" : ""
         }`}
       >
         <div className="flex justify-between w-full">
@@ -93,9 +72,7 @@ export const ContractData = () => {
           </button>
           <div className="flex border bg-secondary border-primary rounded-xl">
             <div className="flex items-end p-2 py-1 border-r border-primary">Total count</div>
-            <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree">
-              {totalCounter?.toString() || "0"}
-            </div>
+            <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree">{"0"}</div>
           </div>
         </div>
 
@@ -103,7 +80,7 @@ export const ContractData = () => {
           <div className="relative overflow-x-hidden" ref={containerRef}>
             {/* for speed calculating purposes */}
             <div className="absolute -left-[9999rem]" ref={greetingRef}>
-              <div className="px-4">{currentGreeting}</div>
+              <div className="px-4">{"0"}</div>
             </div>
             {new Array(3).fill("").map((_, i) => {
               const isLineRightDirection = i % 2 ? isRightDirection : !isRightDirection;
@@ -112,11 +89,10 @@ export const ContractData = () => {
                   key={i}
                   direction={isLineRightDirection ? "right" : "left"}
                   gradient={false}
-                  play={showTransition}
                   speed={marqueeSpeed}
                   className={i % 2 ? "-my-10" : ""}
                 >
-                  <div className="px-4">{currentGreeting || " "}</div>
+                  <div className="px-4">{" "}</div>
                 </Marquee>
               );
             })}
@@ -139,7 +115,7 @@ export const ContractData = () => {
           <div className="w-44 p-0.5 flex items-center bg-neutral border border-primary rounded-full">
             <div
               className="h-1.5 border border-primary rounded-full bg-secondary animate-grow"
-              style={{ animationPlayState: showTransition ? "running" : "paused" }}
+              style={{ animationPlayState: "paused" }}
             />
           </div>
         </div>
